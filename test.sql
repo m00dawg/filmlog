2cda634f3d3a074a5d32c2ea1765e4ec601723e3
Tim Soderstrom <tim@moocowproductions.org>
2017-07-17 22:27:26 -0500

    Cameras test data

--- test.sql
+++ test.sql
@@ -24,8 +24,8 @@ INSERT INTO Projects (name) VALUES ('Black and White Practice');
 INSERT INTO Projects (name) VALUES ('Medium Format Practice');
 INSERT INTO Projects (name) VALUES ('Alaska 2017');
 
-INSERT INTO Cameras VALUES (1, 'Nikon N80', '35mm');
-INSERT INTO Cameras VALUES (2, 'Mamiya 645 1000s', '120');
+INSERT INTO Cameras VALUES (1, '35mm', 'Nikon N80');
+INSERT INTO Cameras VALUES (2, '120', 'Mamiya 645 1000s');
 
 INSERT INTO Lenses VALUES (1, 'Nikon 50mm');
 INSERT INTO Lenses VALUES (2, 'Mamiya 80mm f/2.4');
@@ -41,11 +41,11 @@ INSERT INTO Filters VALUES (1, 'Circular Polarizer', 'CP');
 INSERT INTO Filters vALUES (2, 'Yellow 8', 'YEL8');
 INSERT INTO Filters VALUES (3, 'Red 25', 'RED25');
 
-INSERT INTO Exposures VALUES (1, 1, 1, '125', 8);
+INSERT INTO Exposures VALUES (1, 1, 1, '125', 8, null);
 INSERT INTO ExposureFilters VALUES (1, 1, 1);
 INSERT INTO ExposureFilters VALUES (1, 1, 2);
-INSERT INTO Exposures VALUES (1, 2, 1, '250', 16);
+INSERT INTO Exposures VALUES (1, 2, 1, '250', 16, null);
 INSERT INTO ExposureFilters VALUES (1, 1, 3);
-INSERT INTO Exposures VALUES (1, 3, 1, '1000', 5.8);
-INSERT INTO Exposures VALUES (1, 4, 1, '0', 5.8);
-INSERT INTO Exposures VALUES (1, 5, 1, '-1', 5.8);
+INSERT INTO Exposures VALUES (1, 3, 1, '1000', 5.8, null);
+INSERT INTO Exposures VALUES (1, 4, 1, '0', 5.8, null);
+INSERT INTO Exposures VALUES (1, 5, 1, '-1', 5.8, null);
