66a6a8ef8074982431e5b7b4e2fb240cdf9fd7e7
Tim Soderstrom <tim@moocowproductions.org>
2018-01-25 19:57:05 -0600

    Splitting out film sizes for the stock in a better way

--- filmlog/views.py
+++ filmlog/views.py
@@ -476,17 +476,40 @@ def filmstock():
                             filmTypeID=request.form.get('filmTypeID'),
                             filmSize=request.form.get('filmSize'),
                             qty=request.form.get('qty'))
+    qry = text("""SELECT FilmStock.filmTypeID AS filmTypeID, filmSize, qty,
+        FilmBrands.brand AS brand, FilmTypes.name AS type, iso
+        FROM FilmStock
+        JOIN FilmTypes ON FilmTypes.filmTypeID = FilmStock.filmTypeID
+        JOIN FilmBrands ON FilmBrands.filmBrandID = FilmTypes.filmBrandID
+        WHERE filmSize IN ("35mm 24", "35mm 36", "35mm 100' Bulk Roll")
+        ORDER BY filmSize, brand, type, iso""")
+    stock_35mm = engine.execute(qry).fetchall()
 
     qry = text("""SELECT FilmStock.filmTypeID AS filmTypeID, filmSize, qty,
         FilmBrands.brand AS brand, FilmTypes.name AS type, iso
         FROM FilmStock
         JOIN FilmTypes ON FilmTypes.filmTypeID = FilmStock.filmTypeID
         JOIN FilmBrands ON FilmBrands.filmBrandID = FilmTypes.filmBrandID
+        WHERE filmSize IN ('120', '220')
         ORDER BY filmSize, brand, type, iso""")
-    stock = engine.execute(qry).fetchall()
+    stock_mf = engine.execute(qry).fetchall()
+
+    qry = text("""SELECT FilmStock.filmTypeID AS filmTypeID, filmSize, qty,
+        FilmBrands.brand AS brand, FilmTypes.name AS type, iso
+        FROM FilmStock
+        JOIN FilmTypes ON FilmTypes.filmTypeID = FilmStock.filmTypeID
+        JOIN FilmBrands ON FilmBrands.filmBrandID = FilmTypes.filmBrandID
+        WHERE filmSize IN ('4x5', '8x10')
+        ORDER BY filmSize, brand, type, iso""")
+    stock_sheets = engine.execute(qry).fetchall()
+
     qry = text("""SELECT FilmTypes.filmTypeID AS filmTypeID,
         FilmBrands.brand AS brand, FilmTypes.name AS type, iso
         FROM FilmTypes
         JOIN FilmBrands ON FilmBrands.filmBrandID = FilmTypes.filmBrandID""")
     films = engine.execute(qry).fetchall()
-    return render_template('filmstock.html', stock=stock, films=films)
+    return render_template('filmstock.html',
+                stock_35mm=stock_35mm,
+                stock_mf=stock_mf,
+                stock_sheets=stock_sheets,
+                films=films)
