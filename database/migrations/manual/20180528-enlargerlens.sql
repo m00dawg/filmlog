5a25fb69024b1ef2e91f771582642967bf71a9ef
Tim Soderstrom <tim@moocowproductions.org>
2018-05-28 16:38:34 -0500

    Yeah need that too

--- database/migrations/manual/20180528-enlargerlens.sql
+++ database/migrations/manual/20180528-enlargerlens.sql
@@ -6,6 +6,13 @@ CREATE TABLE EnlargerLenses(
     CONSTRAINT EnlargerLenses_Users_fk FOREIGN KEY (userID) REFERENCES Users (userID) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE='InnoDB';
 
+ALTER TABLE ContactSheets ADD COLUMN enlargerLensID TINYINT UNSIGNED DEFAULT NULL AFTER paperFilterID;
+ALTER TABLE ContactSheets
+ADD CONSTRAINT ContactSheets_EnlargerLenses_fk FOREIGN KEY (userID, enlargerLensID)
+REFERENCES EnlargerLenses (userID, enlargerLensID)
+ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 ALTER TABLE Prints ADD COLUMN enlargerLensID TINYINT UNSIGNED DEFAULT NULL AFTER paperFilterID;
 ALTER TABLE Prints
 ADD CONSTRAINT Prints_EnlargerLenses_fk FOREIGN KEY (userID, enlargerLensID) 
