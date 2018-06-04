998c10ab820b47cbdf6440bf779f0a0a3b765e56
Tim Soderstrom <tim@moocowproductions.org>
2018-06-03 21:41:49 -0500

    Starting work on camera form

--- filmlog/views.py
+++ filmlog/views.py
@@ -15,7 +15,7 @@ from filmlog import app
 from filmlog import database
 from filmlog.functions import next_id, result_to_dict, get_film_details, \
     optional_choices, zero_to_none
-from filmlog import users, filmstock, darkroom, files, stats, help
+from filmlog import users, filmstock, darkroom, files, stats, gear, help
 engine = database.engine
 
 ## Functions
@@ -632,48 +632,3 @@ def expsoure(binderID, projectID, filmID, exposureNumber):
 def filmtypes():
     filmtypes = get_film_types(engine.connect())
     return render_template('filmtypes.html', filmtypes=filmtypes)
-
-@app.route('/gear',  methods = ['GET', 'POST'])
-@login_required
-def gear():
-    connection = engine.connect()
-    transaction = connection.begin()
-    userID = current_user.get_id()
-
-    if request.method == 'POST':
-        app.logger.debug('POST')
-        if request.form['button'] == 'addCamera':
-            app.logger.debug('Add Camera')
-            nextCameraID = next_id(connection, 'cameraID', 'Cameras')
-            qry = text("""INSERT INTO Cameras
-                (cameraID, userID, name, filmSize) VALUES (:cameraID, :userID, :name, :filmSize)""")
-            connection.execute(qry,
-                cameraID = nextCameraID,
-                userID = int(current_user.get_id()),
-                name = request.form['name'],
-                filmSize = request.form['filmSize'],
-                )
-        if request.form['button'] == 'addFilter':
-            nextFilterID = next_id(connection, 'filterID', 'Filters')
-            qry = text("""INSERT INTO Filters
-                (userID, filterID, name, code, factor, ev)
-                VALUES (:userID, :filterID, :name, :code, :factor, :ev)""")
-            connection.execute(qry,
-                userID = userID,
-                filterID = nextFilterID,
-                name = request.form['name'],
-                code = request.form['code'],
-                factor = request.form['factor'],
-                ev = request.form['ev'])
-
-    qry = text("""SELECT cameraID, name, filmSize
-        FROM Cameras
-        WHERE userID = :userID""")
-    cameras = connection.execute(qry, userID = userID).fetchall()
-
-    qry = text("""SELECT filterID, name, code, factor, ev
-                  FROM Filters
-                  WHERE userID = :userID""")
-    filters = connection.execute(qry, userID = current_user.get_id()).fetchall()
-    transaction.commit()
-    return render_template('gear.html', cameras=cameras, filters=filters)
